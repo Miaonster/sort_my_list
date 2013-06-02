582cef4668d8f9e72c3c3cc7cf477c77a966052e
Witcher42 <dy.lengleng@gmail.com>
2013-06-02 18:07:30 +0800

    upload to github

--- test.js
+++ test.js
@@ -1,3 +1,56 @@
 $(document).ready(function() {
-	
+  var $list;
+
+	function init() {
+    $list = $('#sortable-list');
+    if ($.browser.chrome) {
+      //
+    } else if ($.browser.safari) {
+      document.body.setAttribute('contenteditable', true);
+      $('body').removeClass('notice');
+    } else if (!$.browser.chrome) {
+      $('#js-error-notice')
+        .text('Only Chrome / Safari is supported.')
+      return;
+    }
+
+    initEvent();
+    initHome();
+  }
+
+  function initEvent() {
+    document.body.onpaste = function(e) {
+      handleText(e.clipboardData.getData('text/html'));
+    }
+  }
+
+  function initHome() {
+    setTimeout(function() {
+      $('#js-error-notice').fadeOut(400, function() {
+        $('body').removeClass('notice');
+        $('#js-sortable-container').fadeIn(600);
+        $list.sortable();
+      });
+    }, 1200);
+  }
+
+	function handleText (text) {
+    var i,
+        content,
+        html = '',
+        arr = text.match(/<li>.*?<\/li>/ig);
+
+    console.log(text);
+
+    for (i = 0; arr && i < arr.length; i++) {
+      content = arr[i].replace(/<([^>]+).*?>/ig, '');
+      content = '<li class="sortable-item"><div>' + content + '</div></li>';
+      html += content;
+    }
+
+    $list.html(html);
+    $list.sortable();
+	}
+
+  init();
 });
